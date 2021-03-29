7cf62fff33a8f7ec3223add04f912633aad24756
Gianluca Grasso <gianluca.grasso@ynap.com>
2021-03-29 23:55:42 +0200

    no logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <a href="/" className="navbar-item">
-                    <img alt="logo" src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                    Calcolo Punteggio A.T.A.
                 </a>
 
                 
